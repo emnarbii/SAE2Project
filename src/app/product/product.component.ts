d9e097f5cadcf234dc7d75aec0116299b54e6486
emnarbii <emna.rbii@esprit.tn>
2024-11-26 09:47:58 +0100

    data-binding1

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,11 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
-  class="sae2";
-  subject="Angular";
-  color="";
+//   class="sae2";
+//   subject="Angular";
+//   color="";
 
- onClick(){
-  return alert("Vous avez cliquer!!!!!");
- }
+//  onClick(){
+//   return alert("Vous avez cliquer!!!!!");
+//  }
+
+title: string = 'ProductList';
+  color = 'red';
+  productList = [
+    { id: 1, pName: 'PC', price: '2000', img: '../assets/images/pc.jpg' },
+    { id: 2, pName: 'souris', price: '20', img: '../assets/images/souris.png' },
+    { id: 3, pName: 'écran', price: '200', img: '../assets/images/ecran.jpg' },
+  ];
+
+  
+  clickMe() {
+    return alert('vous avez cliquer');
+  }
 }
